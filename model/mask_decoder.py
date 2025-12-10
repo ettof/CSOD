1daaa12ec0a6f06caa0bdfdef4ac1c4a368e2e68
zychen <97585935+ettof@users.noreply.github.com>
2025-12-10 23:05:54 +0800

    Update mask_decoder.py

--- model/mask_decoder.py
+++ model/mask_decoder.py
@@ -180,6 +180,8 @@ class MaskDecoder(nn.Module):
 
         # Generate mask quality predictions
         # iou_pred = self.iou_prediction_head(iou_token_out)
+        
 
         return masks, sparse_prompt, sam_tokens_out
+
 
